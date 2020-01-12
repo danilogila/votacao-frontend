5c8331ea089b594e5c61d4174ff7c228a5bb91e1
Danilo Gila de Santana <danilosantana58@gmail.com>
2020-01-11 22:23:03 -0300

    Add filter to prevent showing closed polls

--- src/components/pages/main/index.js
+++ src/components/pages/main/index.js
@@ -5,24 +5,41 @@ import { Link } from 'react-router-dom'
 import './styles.css'
 
 export default class Main extends Component{
-
-    state = {
-        polls: []
+    constructor(props){
+        super(props)
+        this.state = {
+            polls: []
+        }
     }
 
     componentDidMount(){
         this.loadPolls();
     }
 
+    parseDateString = (date) => {
+        return new Date(date).toLocaleDateString().substring(0,10).replace(/-/g, "/") 
+    }
+
     loadPolls = async () =>{
         const response = await api.get('/polls')
-        this.setState({ polls: response.data })
+        const now = new Date()
+
+        let polls = response.data.filter((poll) => {
+            const remainTime = new Date(poll.closeAt).getTime() - now.getTime();
+            return remainTime > 0
+        })
+
+        this.setState({ polls })
     }
 
     render(){
+        if(this.state.polls.length <= 0){
+            return <h1 className="empty-poll-list-message">Não há votações cadastradas :(</h1>
+        }
+
         return (
             <div className="poll-list">
-                {this.state.polls.map(poll => (
+                {this.state.polls.length > 0 && this.state.polls.map(poll => (
                     <article key={poll._id}>
                         <section className="poll-thumbnail">
                             <picture>
@@ -31,12 +48,12 @@ export default class Main extends Component{
                         </section>
                         <strong className="poll-title">{poll.title}</strong>
                         <p>Criado por: <span>{poll.author}</span></p>
-                        <p>Postado em: <span>{poll.createdAt}</span></p>
+                        <p>Postado em: <span>{this.parseDateString(poll.createdAt)}</span></p>
                         <section className="poll-vote-btn">
                             <Link to={`/polls/${poll._id}`}>Votar</Link>
                         </section>
                     </article>
-                ))}
+                )) }
             </div>
         )
     }
